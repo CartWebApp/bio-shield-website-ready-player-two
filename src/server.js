cf963630762952ee8cc6d0493b5111c8a3fc07d4
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2025-09-12 15:49:44 -0700

    more

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
-import { readFileSync } from 'fs';
+import { readFileSync, existsSync, statSync, stat } from 'fs';
 // @ts-ignore
 import express from 'express';
 
-const app = express();
+const app = /** @type {import('express').Application} */ (express());
 
-app.use(express.static('routes'));
+app.use((req, res, next) => {
+    const path = `./routes${req.path}`;
+    if (existsSync(path)) {
+        const stats = statSync(path);
+        if (stats.isFile()) {
+            console.log(path);
+            res.contentType(`.${path.split('.').at(-1) ?? 'txt'}`);
+            res.sendFile(path, { root: '.'});
+        } else {
+            res.sendFile(`${path}index.html`, { root: '.' });
+        }
+    }
+    // next();
+});
 
 app.listen(3000);
