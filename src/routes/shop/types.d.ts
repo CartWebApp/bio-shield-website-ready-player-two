8a9b80d22e0ee05a54feeedd51363ebfd7d7df82
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2025-09-17 23:53:37 -0700

    fix

--- src/routes/shop/types.d.ts
+++ src/routes/shop/types.d.ts
@@ -7,4 +7,8 @@ declare global {
 	};
 	export function useContext<K extends keyof Context>(key: K): Context[K];
 	export function useContext(): Context;
+	interface Params {
+	};
+	export function useParams<P extends keyof Params>(param: P): Params[P];
+	export function useParams(): Params;
 };
