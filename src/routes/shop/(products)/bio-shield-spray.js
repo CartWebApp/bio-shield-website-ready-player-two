d991292fce875622c3c6b937af40bc4703e0a901
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2025-09-18 23:30:27 -0700

    fix naming

--- src/routes/shop/(products)/bio-shield-spray.js
+++ src/routes/shop/(products)/bio-shield-spray.js
@@ -1,7 +1,7 @@
 /** @import { Product } from '#types' */
 
 export default /** @type {Product} */ ({
-    name: 'Bio Shield Spray',
+    name: 'Bio-Shield Spray',
     images: [],
     description: '',
     price: 4.99
