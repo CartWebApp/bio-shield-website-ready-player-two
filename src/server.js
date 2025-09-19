4a63f0f133935941f572c42a2c42eda23008f084
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2025-09-19 15:01:16 -0700

    logging

--- src/server.js
+++ src/server.js
@@ -366,7 +366,7 @@ app.use(async (req, res, next) => {
     const path = `./src/routes${req.path}`;
     const prefetching = typeof req.query.prefetching === 'string';
     console.log(path);
-    console.log(readdirSync('./src', { recursive: true }));
+    console.log(readdirSync('.', { recursive: true }));
     if (existsSync(path)) {
         console.log('exists');
         const stats = statSync(path);
