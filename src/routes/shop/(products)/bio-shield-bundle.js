990237a4dc274d84216c09ce35a3609e1e24da6c
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2025-09-20 02:01:03 -0700

    add scrolling to product image viewer

--- src/routes/shop/(products)/bio-shield-bundle.js
+++ src/routes/shop/(products)/bio-shield-bundle.js
@@ -2,7 +2,14 @@
 
 export default /** @type {Product} */ ({
     name: 'Bio-Shield Bundle',
-    images: ['/static/bundle-deal.png'],
+    images: [
+        '/static/bundle-deal.png',
+        '/static/bracelet.png',
+        '/static/salve.png',
+        '/static/spray.png',
+        '/static/spray-advert.png',
+        '/static/example-spray-upscaled.png'
+    ],
     description: '',
     price: 15.99
 });
