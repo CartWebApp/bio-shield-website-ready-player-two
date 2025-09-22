98d92133f96f7e03e80470d4501b5f5698fb3244
DavidKozlov <DavidKozlov24@gmail.com>
2025-09-22 13:22:36 -0700

    Almost done

--- src/routes/shop/(products)/bio-shield-bundle.js
+++ src/routes/shop/(products)/bio-shield-bundle.js
@@ -7,9 +7,7 @@ export default /** @type {Product} */ ({
         '/static/bracelet.webp',
         '/static/salve.webp',
         '/static/spray.webp',
-        '/static/spray-advert.webp',
-        '/static/example-spray-upscaled.png'
     ],
-    description: '',
+    description: 'Enjoy nature without the worry of bugs with the Bio-Shield Complete Protection Bundle—a trio of our best-selling, eco-friendly insect repellents. Thoughtfully crafted with plant-based, biodegradable ingredients, this set gives you flexible, natural protection for every outdoor adventure.',
     price: 15.99
 });
