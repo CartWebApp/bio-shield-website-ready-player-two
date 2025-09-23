16ed1ddf5d3f30da41ac2afc07a744c2997f2b5a
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2025-09-22 20:15:01 -0700

    add build step, improve styles

--- src/routes/+error/+client.js
+++ src/routes/+error/+client.js
@@ -1,7 +1,7 @@
 /// <reference path="./types.d.ts" />
 const { error } = useContext();
 const h1 = /** @type {HTMLHeadingElement} */ (document.querySelector('h1'));
-const h5 = /** @type {HTMLHeadingElement} */ (document.querySelector('h5'));
+const h4 = /** @type {HTMLHeadingElement} */ (document.querySelector('h4'));
 h1.textContent = error.status.toString();
-h5.textContent = error.message;
+h4.textContent = error.message;
 document.title = `${error.status} - Bio-Shield`;
---
+++ src/server/build.js
@@ -0,0 +1,108 @@
+// @ts-check
+import {
+    readdirSync,
+    writeFileSync,
+    readFileSync,
+    cpSync,
+    mkdirSync,
+    existsSync
+} from 'fs';
+import { transform } from 'lightningcss';
+import { join, parse, sep } from 'path';
+import { minify } from 'terser';
+
+if (!existsSync(join(process.cwd(), 'src', 'build'))) {
+    mkdirSync(join(process.cwd(), 'src', 'build'));
+}
+
+for (const file of readdirSync(join(process.cwd(), 'src', 'routes'), {
+    withFileTypes: true,
+    recursive: true
+})) {
+    if (!file.isFile()) {
+        continue;
+    }
+    const parsed = parse(file.name);
+    if (
+        (parsed.ext !== '.js' || file.name.endsWith('.remote.js')) &&
+        parsed.ext !== '.css'
+    ) {
+        cpSync(
+            join(file.parentPath, file.name),
+
+            join(
+                ...`${file.parentPath
+                    .replaceAll('/', sep)
+                    .replace(
+                        join(process.cwd(), 'src', 'routes'),
+                        join(process.cwd(), 'src', 'build')
+                    )}${sep}${file.name}`.split('/')
+            ),
+            { recursive: true }
+        );
+        continue;
+    }
+    if (parsed.ext === '.js') {
+        const contents = readFileSync(
+            join(file.parentPath, file.name),
+            'utf-8'
+        );
+        const minified = await minify(contents, {
+            module: true
+        });
+        if (
+            !existsSync(
+                join(
+                    file.parentPath
+                        .replaceAll('/', sep)
+                        .replace(
+                            join(process.cwd(), 'src', 'routes'),
+                            join(process.cwd(), 'src', 'build')
+                        )
+                )
+            )
+        ) {
+            mkdirSync(
+                join(
+                    file.parentPath
+                        .replaceAll('/', sep)
+                        .replace(
+                            join(process.cwd(), 'src', 'routes'),
+                            join(process.cwd(), 'src', 'build')
+                        )
+                ),
+                { recursive: true }
+            );
+        }
+        writeFileSync(
+            join(
+                ...`${file.parentPath
+                    .replaceAll('/', sep)
+                    .replace(
+                        join(process.cwd(), 'src', 'routes'),
+                        join(process.cwd(), 'src', 'build')
+                    )}${sep}${file.name}`.split('/')
+            ),
+            minified.code ?? ''
+        );
+    } else {
+        const contents = readFileSync(join(file.parentPath, file.name));
+        const minified = transform({
+            code: contents,
+            filename: join(file.parentPath, file.name),
+            minify: true
+        });
+        writeFileSync(
+            join(
+                ...`${file.parentPath
+                    .replaceAll('/', sep)
+                    .replace(
+                        join(process.cwd(), 'src', 'routes'),
+                        join(process.cwd(), 'src', 'build')
+                    )}${sep}${file.name}`.split('/')
+            ),
+
+            minified.code
+        );
+    }
+}
